326679dbe63dd0454f2be2f97497aa6ca2cb4592
Roberto Polli <robipolli@gmail.com>
2023-01-12 18:41:10 +0100

    Add further tools. (#2)
    * Fix: #1. Semgrep & co. using python entrypoint.

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,9 @@ FROM openpolicyagent/conftest:v0.36.0 as conftest
 FROM jenkins/jnlp-agent-maven as maven
 RUN mkdir /app/java-validators -p
 COPY pom.xml /app/java-validators
-RUN --mount=type=cache,target=/app/java-validators/.m2 sh -c '(cd /app/java-validators &&  \
-                mvn -Duser.home=/app/java-validators package && \
-                cp -r /app/java-validators/.m2 /root/.m2)'
+RUN --mount=type=cache,target=/root/.m2 sh -c '(cd /app/java-validators &&  \
+                mvn package && \
+                cp -r /root/.m2 /app/java-validators/)'
 
 
 # Base image
@@ -26,6 +26,7 @@ ARG TALISMAN_VERSION=v1.29.4
 ARG TALISMAN_URL=https://github.com/thoughtworks/talisman/releases/download/${TALISMAN_VERSION}/talisman_linux_386
 ARG KUBESCAPE_VERSION=v2.0.180
 ARG KUBESCAPE_URL=https://github.com/kubescape/kubescape/releases/download/${KUBESCAPE_VERSION}/kubescape-ubuntu-latest
+
 
 RUN apk add --no-cache \
 	openjdk11-jre \
@@ -46,7 +47,6 @@ COPY --from=trivy /usr/local/bin/trivy /usr/bin/trivy
 RUN mkdir -p /usr/share
 COPY --from=maven /usr/share/maven /usr/share/maven
 COPY --from=maven /app/java-validators /app/java-validators
-COPY --from=maven /root/.m2 /app/java-validators/.m2
 RUN ln -s  /usr/share/maven/bin/mvn /usr/bin/mvn
 
 # Install conftest.
@@ -56,19 +56,31 @@ COPY --from=conftest /usr/local/bin/conftest /usr/bin/conftest
 RUN pip3 install tox==${TOX_VERSION} \
         pre-commit==${PRE_COMMIT_VERSION} \
         pytest==${PYTEST_VERSION}
+COPY requirements.txt /app/
+RUN pip3 install -r /app/requirements.txt
 
+COPY config /app/config/
+COPY entrypoint.py /
 
+#
+# Default variables.
+#
+# Dowload maven deps to /tmp/.m2/
+ENV M2_HOME=/tmp
 
-COPY entrypoint.sh /
+# Read-only checks
+ENV RUN_SPOTBUGS=true
+ENV RUN_OWASP_DEPENDENCY_CHECK=true
+ENV RUN_TRIVY_FILESYSTEM=true
+ENV RUN_TRIVY_CONFIG=true
+ENV RUN_SPOTLESS_CHECK=true
+ENV RUN_CONFTEST=true
+ENV RUN_KUBESCAPE=true
+ENV RUN_TALISMAN=true
 
-ENV CHECK_SPOTBUGS=true
-ENV CHECK_OWASP_DEPENDENCY_CHECK=true
-ENV CHECK_TRIVY=true
-ENV CHECK_SPOTLESS=true
-ENV CHECK_SPOTLESS_GOAL=check
-ENV CHECK_CONFTEST=true
-ENV CHECK_KUBESCAPE=true
-ENV CHECK_TALISMAN=true
+# Write checks.
+ENV RUN_SPOTLESS_APPLY=false
 
 USER 1000
-ENTRYPOINT ["/entrypoint.sh"]
+HEALTHCHECK NONE
+ENTRYPOINT ["python", "/entrypoint.py"]
---
+++ entrypoint.py
@@ -0,0 +1,196 @@
+"""
+Run various SAST tools.
+"""
+import logging
+import os
+import shlex
+import subprocess
+from argparse import ArgumentParser
+from pathlib import Path
+from sys import stderr, stdout
+
+# Log to stdout
+# for both stdout and stderr.
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    handlers=[
+        logging.StreamHandler(),
+    ],
+)
+log = logging.getLogger(__name__)
+
+TOOLS_MAP = {
+    "trivy_config": {
+        "cmdline": "trivy --config {config_file} config .",
+        "config_file": "trivy.yaml",
+    },
+    "trivy_filesystem": {
+        "cmdline": "trivy --config {config_file} filesystem .",
+        "config_file": "trivy.yaml",
+    },
+    "bandit": {
+        "cmdline": "bandit --config {config_file} {args} -r .",
+        "config_file": "bandit.yaml",
+    },
+    "safety": {
+        "cmdline": "safety {args} check",
+        "config_file": "safety.yaml",
+    },
+    "kubescape": {
+        "cmdline": "kubescape {args} scan .",
+        "args": "--cache-dir /tmp",
+    },
+    "checkov": {
+        "cmdline": "checkov --config-file {config_file} {args} --directory .",
+        "config_file": ".checkov.yaml",
+    },
+    "semgrep": {
+        "cmdline": "semgrep --config {config_file} {args} .",
+        "config_file": "string://auto",
+    },
+    "spotbugs": {
+        "cmdline": "mvn com.github.spotbugs:spotbugs-maven-plugin:check",
+    },
+    "owasp_dependency_check": {
+        "cmdline": "mvn org.owasp:dependency-check-maven:check",
+    },
+    "spotless_check": {
+        "cmdline": "mvn com.diffplug.spotless:spotless-maven-plugin:check",
+    },
+    "spotless_apply": {
+        "cmdline": "mvn com.diffplug.spotless:spotless-maven-plugin:apply",
+    },
+    #  TODO: conftest
+    #  TODO: talisman
+}
+
+
+def _localize(path, config_dir):
+    if not path:
+        return ""
+    if path.startswith("string://"):
+        return path[9:]
+    return (config_dir / path).absolute().as_posix()
+
+
+def run_sast(tool, command, env, config_dir, log_file=stdout):
+    log.info(f"Preparing {tool}")
+
+    var_enabled = f"RUN_{tool.upper()}"
+    var_args = f"{tool.upper()}_ARGS"
+    var_config_file = f"{tool.upper()}_CONFIG_FILE"
+
+    env_enabled = env.get(var_enabled, "true")
+    env_args = env.get(var_args, "")
+    env_config_file = env.get(var_config_file, "")
+
+    default_args = command.get("args", "")
+    default_config_file = _localize(command.get("config_file", ""), config_dir)
+
+    if env_enabled.lower() != "true":
+        log.info(f"Skipping {tool}")
+        return
+
+    cmdline = command["cmdline"]
+    config_file = env_config_file or default_config_file
+    cmd_args = env_args or default_args
+    cmd = cmdline.format(
+        args=cmd_args,
+        config_file=config_file,
+    )
+    log.info(f"Running {cmd}")
+
+    status = subprocess.run(
+        shlex.split(cmd),
+        shell=False,
+        stdout=log_file,
+        stderr=log_file,
+        check=False,  # Don't raise exception on non-zero exit code.
+        timeout=600,
+        env=env,
+    )
+    if status.returncode != 0:
+        log.error(f"{tool} failed with status {status.returncode}")
+    return status.returncode
+
+
+def _show_environ(config_dir, dump_config=False):
+    env = os.environ
+    print("Environment variables:")
+    print("""|Variable|Default|Tool|""")
+    print("""|--------|-------|----|""")
+    for tool, command in TOOLS_MAP.items():
+        var_enabled = f"RUN_{tool.upper()}"
+        var_args = f"{tool.upper()}_ARGS"
+        var_config_file = f"{tool.upper()}_CONFIG_FILE"
+
+        env_enabled = env.get(var_enabled, "true")
+        env_args = env.get(var_args, "")
+        env_config_file = env.get(var_config_file, "")
+
+        default_args = command.get("args", "")
+        default_config_file = _localize(
+            command.get("config_file", ""), config_dir=config_dir
+        )
+        if dump_config:
+            print(
+                f"""
+            # {tool}
+            {var_enabled}={env_enabled}
+            {var_args}={env_args or default_args}  # {default_args}
+            {var_config_file}="{env_config_file or default_config_file}"  # {default_config_file}
+            """
+            )
+            continue
+        print(f"""|{var_enabled}|{env_enabled}|{tool}|""")
+        if default_args:
+            print(f"""|{var_args}|{default_args}|{tool}|""")
+        if default_config_file:
+            print(f"""|{var_config_file}|{default_config_file}|{tool}|""")
+    exit(2)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--config-dir",
+        help="Directory containing config files",
+        default="/app/config",
+    )
+    parser.add_argument(
+        "--environs",
+        help="Environment variables to pass to the tools",
+        default="",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--dump-config",
+        help="Environment variables to pass to the tools",
+        default="",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    if args.environs or args.dump_config:
+        _show_environ(Path(args.config_dir), dump_config=args.dump_config)
+    tee = subprocess.Popen(
+        ["/usr/bin/ tee", "super-sast.log"], shell=False, stdin=subprocess.PIPE
+    )
+    # Cause tee's stdin to get a copy of our stdin/stdout (as well as that
+    # of any child processes we spawn)
+    os.dup2(tee.stdin.fileno(), stdout.fileno())
+    os.dup2(tee.stdin.fileno(), stderr.fileno())
+    sast_status = {}
+    for tool, command in TOOLS_MAP.items():
+        status = run_sast(
+            tool,
+            command,
+            os.environ.copy(),
+            config_dir=Path(args.config_dir),
+            log_file=stdout,
+        )
+        sast_status[tool] = status
+    log.info("All tools finished")
+    log.info(sast_status)
