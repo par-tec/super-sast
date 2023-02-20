1bbae26d3290348c1fa2859a1bfa421919c7b7a5
Mattia <hackhab@gmail.com>
2023-02-20 10:33:11 +0100

    Fixes #27 (#31)
    * Reverted CI-breaking changes and fixed pre-commit configuration
    
    * Don't hide failure status.
    
    ---------
    
    Co-authored-by: Roberto Polli <robipolli@gmail.com>

--- entrypoint.py
+++ entrypoint.py
@@ -13,6 +13,7 @@ import os
 import re
 import shlex
 import subprocess
+import sys
 import uuid
 from argparse import ArgumentParser
 from pathlib import Path
@@ -294,7 +295,7 @@ def _show_environ(config_dir, dump_config=False):
             print(f"""|{var_args}|{default_args}|{tool}|""")
         if default_config_file:
             print(f"""|{var_config_file}|{default_config_file}|{tool}|""")
-    exit(2)
+    sys.exit(2)
 
 
 def _copy_java_validators():
@@ -303,7 +304,18 @@ def _copy_java_validators():
         log.warning(f"Directory {m2_home} already exists. Skipping copy.")
         return
     log.info(f"Copying java validators to {m2_home.absolute().as_posix()}")
-    copytree("/app/java-validators/.m2", m2_home)
+    try:
+        copytree("/app/java-validators/.m2", m2_home)
+    except Exception as e:
+        stat = os.stat(m2_home)
+        log.error(
+            "An exception occurred while copying java validators, context information below\n"
+            f" - UID: {os.getuid()}\n - GID: {os.getgid()}\n"
+            f" - Extra info about the error:\n  - Path: {m2_home.absolute().as_posix()}\n  - Permissions: {oct(stat.st_mode)[2:]}\n"
+            f"  - Owner (user): {stat.st_uid} {f'({usr})' if (usr := os.getenv('USER')) else ''}\n  - Owner (group): {stat.st_gid}\n"
+            f"  - CWD: {os.getenv('PWD', '')}\n"
+            f" - Error -> {type(e).__name__}: {e}"
+        )
 
 
 if __name__ == "__main__":
@@ -326,29 +338,36 @@ if __name__ == "__main__":
         action="store_true",
     )
     args = parser.parse_args()
-
-    if args.environs or args.dump_config:
-        _show_environ(Path(args.config_dir), dump_config=args.dump_config)
-    tee = subprocess.Popen(
-        ["/usr/bin/tee", "super-sast.log"], shell=False, stdin=subprocess.PIPE
-    )
-    # Cause tee's stdin to get a copy of our stdin/stdout (as well as that
-    # of any child processes we spawn)
-    os.dup2(tee.stdin.fileno(), stdout.fileno())
-    os.dup2(tee.stdin.fileno(), stderr.fileno())
-    sast_status = {}
-
-    _copy_java_validators()
-    run_all = os.environ.get("RUN_ALL_TOOLS", "true").lower() == "true"
-    for tool, command in TOOLS_MAP.items():
-        status = run_sast(
-            tool,
-            command,
-            os.environ.copy(),
-            config_dir=Path(args.config_dir),
-            log_file=stdout,
-            run_all=run_all,
+    try:
+        if args.environs or args.dump_config:
+            _show_environ(Path(args.config_dir), dump_config=args.dump_config)
+        tee = subprocess.Popen(
+            ["/usr/bin/tee", "super-sast.log"], shell=False, stdin=subprocess.PIPE
         )
-        sast_status[tool] = status
-    log.info("All tools finished")
-    log.info(sast_status)
+        # Cause tee's stdin to get a copy of our stdin/stdout (as well as that
+        # of any child processes we spawn)
+        os.dup2(tee.stdin.fileno(), stdout.fileno())
+        os.dup2(tee.stdin.fileno(), stderr.fileno())
+        sast_status = {}
+
+        _copy_java_validators()
+        run_all = os.environ.get("RUN_ALL_TOOLS", "true").lower() == "true"
+        for tool, command in TOOLS_MAP.items():
+            status = run_sast(
+                tool,
+                command,
+                os.environ.copy(),
+                config_dir=Path(args.config_dir),
+                log_file=stdout,
+                run_all=run_all,
+            )
+            sast_status[tool] = status
+        log.info("All tools finished")
+        log.info(sast_status)
+    except Exception as e:
+        log.error(
+            "An exception occurred while running SAST, context information below\n"
+            f" - UID: {os.getuid()}\n - GID: {os.getgid()}\n"
+            f" - Error: {type(e).__name__} -> {e}"
+        )
+        raise e
